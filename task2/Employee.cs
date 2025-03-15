aef63d41b7ae0fcc9424f5a90f1af29ab19e67f5
hala mohammed <halahanem767@gmail.com>
2025-03-15 20:50:53 +0200

    programmoopAdd project files.

---
+++ task2/Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+
+    public enum Securityprevilige
+    {
+        guest,
+        Developer,
+        secretary,
+        DBA
+
+    }
+
+    internal class Employee
+    {
+        private char gender;
+        public string Name {  get; set; }
+        public int Id {  get; set; }
+        public double Salary {  get; set; }
+        public DateTime HireDate {  get; set; }
+        public Securityprevilige SecurityLevel {  get; set; }
+        public char Gender
+        {
+            get 
+            {
+               return gender;
+                }
+            set
+            {
+           
+                
+                if (value == 'F' || value == 'M')
+                    gender = value;
+                else
+                    Console.WriteLine("This is Not valid ,it must be F/M");
+            }
+
+            
+        }
+
+        //CONSTRCUTOR
+        public Employee(string name,int id,double salary,char gender,DateTime hiredate,
+            Securityprevilige securitylevel)
+        {
+            Name = name;
+            Id = id;
+            Salary = salary;
+            Gender = gender;
+            HireDate = hiredate;
+            SecurityLevel = securitylevel;
+             
+        }
+        public override string ToString()
+        {
+
+            return $"ID: {this.Id}\nName: {this.Name}\nSalary: {string.Format("{0:C}", this.Salary)}\nGender: {this.Gender}\n" +
+                $"Hire Date: {this.HireDate}\nSecurity Level: {this.SecurityLevel}";
+
+
+        
+
+        }
+
+    }
+
+     
+    
+}
+
+
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,16 +1,19 @@
 ﻿using task2;
 
 
+#region student
+
 Console.WriteLine("Enter number of students");
 int num = int.Parse(Console.ReadLine());
-
 Student[] g2 = new Student[num];
 
-Student.Promptdetails_students(num, g2);  
+Student.Promptdetails_students(num, g2);
 Student.Display_studentdetails(num, g2);
 
+#endregion
 Console.WriteLine("--------------------------------------------------");
 
+#region post graduates
 PostGraduate postGraduate = new PostGraduate();      // one only for exmaple 
 postGraduate.Name = "amira";
 postGraduate.Age = 22;
@@ -19,8 +22,16 @@ postGraduate.ThesisTopics = "machine learning";
 
 postGraduate.PrintDetails();
 
+#endregion
 
 
+#region employee
+Console.WriteLine("----------------------------------");
+Console.WriteLine("Employee part");
+Employee e = new Employee("ahmed", 3, 56789, 'M', new DateTime(2025,5,15),Securityprevilige.Developer);
+Console.WriteLine(e);
+
+#endregion
 
 
 
